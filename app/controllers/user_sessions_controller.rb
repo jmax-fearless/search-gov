6fd332f4266d6ff7a454df893d46dc249e6f2085
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2020-03-29 12:36:02 -0400

    Rebased onto master

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
+
 class UserSessionsController < ApplicationController
-  before_action :reset_session, only: [:destroy]
   before_action :require_user, only: :destroy
 
   def security_notification
@@ -7,7 +8,39 @@ class UserSessionsController < ApplicationController
   end
 
   def destroy
+    id_token= session[:id_token]
+    login_dot_gov_host= 'https://idp.int.identitysandbox.gov'
+    login_dot_gov_logout_endpoint= '/openid_connect/logout'
+    login_dot_gov_logout_url= "#{login_dot_gov_host}#{login_dot_gov_logout_endpoint}"
+
+    # login_dot_gov_logout_url= 'https://idp.int.identitysandbox.gov/openid_connect/logout'
+    # response= Faraday.get(
+    #   login_dot_gov_logout_url,
+    #   id_token_hint: id_token,
+    #   post_logout_redirect_uri: 'http://localhost:3000/auth/logindotgov/callback',
+    #   state: '1234567890123456789012'
+    # )
+    puts "id_token: #{id_token}"
+
+    connection= Faraday.new(login_dot_gov_logout_url) do |faraday|
+      faraday.request :url_encoded
+      faraday.response :logger
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response= connection.get do |request|
+      request.params['id_token_hint']=  id_token
+      request.params['post_logout_redirect_uri']= 'http://localhost:3000/auth/logindotgov/callback'
+      request.params['state']= '1234567890123456789012'
+    end
+
+    # DEBUG
+    puts "response status: #{response.status}"
+    puts "response body: #{response.body}"
+
+    reset_session
     current_user_session.destroy
     redirect_to(login_path)
+    # redirect_to('http://localhost:3000/auth/logindotgov/callback')
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     client_id: Rails.application.secrets.login_dot_gov[:client_id],
     idp_base_url: Rails.application.secrets.login_dot_gov[:idp_base_url],
     ial: 1,
-    private_key: OpenSSL::PKey::RSA.new(File.read('config/logindotgov.pem')),
+    # private_key: OpenSSL::PKey::RSA.new(File.read('config/logindotgov.pem')),
+    private_key: OpenSSL::PKey::RSA.new(File.read('config/jmax_search_dev_private.pem')),
     redirect_uri: "#{protocol}#{Rails.application.secrets.login_dot_gov[:host]}/auth/logindotgov/callback"
   }
 end
